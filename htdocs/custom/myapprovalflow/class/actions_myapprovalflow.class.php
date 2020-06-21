6cb005cfeab0249d6213486f5f5550e6bf09a07f
mustafa-quick <mbattaloglufl@gmail.com>
2020-06-21 10:38:02 +0300

    approve, disapprove button show, if user is in the flow, and his turn.

--- htdocs/custom/myapprovalflow/class/actions_myapprovalflow.class.php
+++ htdocs/custom/myapprovalflow/class/actions_myapprovalflow.class.php
@@ -26,6 +26,10 @@
 /**
  * Class ActionsMyApprovalFlow
  */
+
+dol_include_once('/myapprovalflow/class/poapprover.class.php');
+require_once DOL_DOCUMENT_ROOT .'/user/class/user.class.php';
+
 class ActionsMyApprovalFlow
 {
     /**
@@ -84,33 +88,61 @@ class ActionsMyApprovalFlow
     }
 
     /**
-     * add email info to notify list
+     * get User Object
      */
-    protected function _addEmailToNotify(&$notify, $apprName)
+    protected function _getUserFromAcct($apprName)
     {
         $user = new User($this->db);
-        $user->fetchAll('ASC','t.rowid',100, 0, array("customsql" => "concat(t.firstname, ' ', t.lastname) like '$apprName'"));
+        $user->fetchAll('ASC','t.rowid', 1, 0, array("customsql" => "concat(t.firstname, ' ', t.lastname) like '$apprName'"));
         if ($user->users == NULL)
-            return -1; // No Approver
+            return NULL; // No Approver
         
         foreach ($user->users as $item) {
-            if ($notify[$item->login] == NULL) {
-                $notify[$item->login] = array(
-                    'email' => $item->email,
-                    'lines' => array()
-                );
-            }
-            $notify[$item->login]['lines'][] = $line->product_ref;                        
+            return $item;                        
         }
+    }
+
+    /**
+     * add email info to notify list
+     */
+    protected function _addEmailToNotify(&$notify, $apprName, $line)
+    {
+        $user = $this->_getUserFromAcct($apprName);
+        if($user == NULL)
+            return -1; // No Approver;
+
+        if ($notify[$user->login] == NULL) {
+            $notify[$user->login] = array(
+                'email' => $user->email,
+                'lines' => array()
+            );
+        }
+        $notify[$user->login]['lines'][] = $line->product_ref;                        
         return 1; // Add successful
     }
+
+    /**
+     * get POApprover Object
+     */
+    protected function _getPOApprover($line)
+    {
+        $pref = preg_replace("/\s+/", "", $line->product_ref);
+        $approvers = new POApprover($this->db);
+        $appuserInfos = $approvers->fetchAll('ASC', 't.rowid', 1, 0, array('t.ref' => $pref));
+
+        foreach( $appuserInfos as $item) {
+            return $item;                    
+        }
+        return NULL;
+    }
+
     /**
      * when submit the proposal, init all datas for workflow
      */
     protected function _initWorkflow(&$object)
     {
         global $conf, $user, $langs;
-        $object->extraparams = array(
+        $extraparams = array(
             "step" => 1,
             "line_stat" => array(
                 "total" => 0,   // total lines for current approval step
@@ -121,22 +153,69 @@ class ActionsMyApprovalFlow
         foreach( $object->lines as $line) {
             if ($line->product_ref != NULL) // only predefined line will go through work flow
             {
-                dol_include_once('/myapprovalflow/class/poapprover.class.php');
-                require_once DOL_DOCUMENT_ROOT .'/user/class/user.class.php';
-                $line->special_code = -1; // 1: approve,  -1: disapprove, 0: undefined
-                $pref = preg_replace("/\s+/", "", $line->product_ref);
-                $approvers = new POApprover($this->db);
-                $appuserInfos = $approvers->fetchAll('ASC', 't.rowid', 1, 0, array('t.ref' => $pref));
-                foreach( $appuserInfos as $ai) {
-                    $this->_addEmailToNotify($notifyList, $ai->app1);
-                    if ($ai->orapp1) // if alternative approver exists?
-                        $this->_addEmailToNotify($notifyList, $ai->orapp1);                    
+                $line->special_code = -1; // 1: approve, 0: disapprove,  -1: undefined
+                $approverObj = $this->_getPOApprover($line);
+                if ($approverObj != NULL) {
+                    $this->_addEmailToNotify($notifyList, $approverObj->app1, $line);
+                    if ($approverObj->orapp1) // if alternative approver exists?
+                        $this->_addEmailToNotify($notifyList, $approverObj->orapp1, $line);
+                }
+                $extraparams['line_stat']['total'] ++;
+                $line->update();
+            }
+        }
+
+        var_dump($notifyList);
+        $object->extraparams = $extraparams;
+        $object->setExtraParameters();
+    }
+
+    /**
+     * check for approve/reject permsion for user
+     */
+    protected function _checkApprovePermission(&$object)
+    {
+        global $conf, $user, $langs;
+        foreach( $object->lines as $line) {
+            if ($line->product_ref != NULL) // only predefined line will go through work flow
+            {
+                if ($line->special_code == -1)                    
+                {
+                    $approverObj = $this->_getPOApprover($line);
+                    switch ($object->extraparams['step'])
+                    {
+                        case 1:
+                            $appUser = $this->_getUserFromAcct($approverObj->app1);
+                            if ($appUser && $appUser->login == $user->login)
+                                return true;
+                            $appUser = $this->_getUserFromAcct($approverObj->orapp1);
+                            if ($appUser && $appUser->login == $user->login)
+                                return true;
+                            break;
+                        case 2;
+                            $appUser = $this->_getUserFromAcct($approverObj->app2);
+                            if ($appUser && $appUser->login == $user->login)
+                                return true;
+                            break;
+                        case 3:
+                            $appUser = $this->_getUserFromAcct($approverObj->app3);
+                            if ($appUser && $appUser->login == $user->login)
+                                return true;
+                            break;
+                        case 4:                            
+                            $appUser = $this->_getUserFromAcct($approverObj->app4);
+                            if ($appUser && $appUser->login == $user->login)
+                                return true;
+                            break;
+                        default:
+                            break;
+                    }
                 }
             }
         }
-        var_dump($notifyList);
-        $object->updateCommon($user);
+        return false;
     }
+
     /**
      * Overloading the doActions function : replacing the parent's function with the one below
      *
@@ -158,26 +237,41 @@ class ActionsMyApprovalFlow
         $error = 0; // Error counter
         /* print_r($parameters); print_r($object); echo "action: " . $action; */
         $context = explode(':', $parameters['context']);
-		if (in_array('ordersuppliercard', $context) && $action == "valid") {
+		if (in_array('ordersuppliercard', $context)) {
 
 			/**
 			* before create proposal, first get accounts for each lines, and send notify to step-1 approvers
-			*/
-			$this->_initWorkflow($object);
+            */
+            switch($action)
+            {
+                case "valid":
+                    $this->_initWorkflow($object);
+                    return 1;
+                    break;
+                case 'lineapprove':
+                    echo "action: " . $action . "<br>";
+                    break;
+                case 'linereject':
+                    echo "action: " . $action . "<br>";
+                    break;
+                default:
+                    echo "action: " . $action . "<br>";
+                    break;
+            }
 
 			/**
 			* get user list example
 			*/
-			require_once DOL_DOCUMENT_ROOT .'/user/class/user.class.php';
-			$usergroup = new User($this->db);
-			$usergroup->fetchAll('ASC','t.rowid',  100, 0, array("customsql" => "t.login like 'mustafa'"));
+			//require_once DOL_DOCUMENT_ROOT .'/user/class/user.class.php';
+			//$usergroup = new User($this->db);
+			//$usergroup->fetchAll('ASC','t.rowid',  100, 0, array("customsql" => "t.login like 'mustafa'"));
 			//var_dump($usergroup->users);
 			//echo "<br><br><br>";
 			/**
 			* send mail example
 			*/
-			include_once DOL_DOCUMENT_ROOT.'/core/class/CMailFile.class.php';
-			$mail = new CMailFile('test', 'postmaster@localhost', 'test@localhost', 'hello', array(), array(), array(), '', '', 0, '', '', '', '', '', 'standard');
+			//include_once DOL_DOCUMENT_ROOT.'/core/class/CMailFile.class.php';
+			//$mail = new CMailFile('test', 'postmaster@localhost', 'test@localhost', 'hello', array(), array(), array(), '', '', 0, '', '', '', '', '', 'standard');
 
 			//$result = $mail->check_server_port('localhost', 25);
 			//var_dump($result);
@@ -220,10 +314,10 @@ class ActionsMyApprovalFlow
         require_once DOL_DOCUMENT_ROOT .'/compta/facture/class/facture.class.php';
 	    $context = explode(':', $parameters['context']);
 		if (in_array('ordersuppliercard', $context)) {
-			if ($object->statut == Facture::STATUS_VALIDATED) {
-				print '<a class="butAction" href="'.$_SERVER["PHP_SELF"].'?id='.$object->id.'&amp;action=approve">'.$langs->trans("Ok, Approve").'</a>';
-				print '<a class="butAction" href="'.$_SERVER["PHP_SELF"].'?id='.$object->id.'&amp;action=reopen">'.$langs->trans("No, Disapprove").'</a>';
-				return 0;
+			if ($object->statut == Facture::STATUS_VALIDATED && $this->_checkApprovePermission($object)) {
+				print '<a class="butAction" href="'.$_SERVER["PHP_SELF"].'?id='.$object->id.'&amp;action=lineapprove">'.$langs->trans("Ok, Approve").'</a>';
+				print '<a class="butAction" href="'.$_SERVER["PHP_SELF"].'?id='.$object->id.'&amp;action=linereject">'.$langs->trans("No, Disapprove").'</a>';
+				return 1;
 			}
 		}
 		return 0;
